640d1b7d772866713b90794f4692f9f4d0cec3d0
Rafiq-1312385 <1312385@my.brunel.ac.uk>
2016-10-17 18:47:03 +0100

    CSV reader working and data passed in correct format

--- src/MovieDataLayer/MovieDataLayer.java
+++ src/MovieDataLayer/MovieDataLayer.java
@@ -5,26 +5,34 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import MovieClassLayer.Actor;
+import MovieClassLayer.Director;
 import MovieClassLayer.Film;
 
 public class MovieDataLayer {
 	static int i=0;
 	Film film= new Film();
 	List<Film> flims;
-	public String Film_ID;
-	public String Film_Name;
-	public String IMDB_Rating;
-	public String Director_ID;
-	public String Director_Nane;
-	public String Actor_ID;
-	public String Actor_Name;
-	public String Year;
+	public static String Film_ID;
+	public static String Film_Name;
+	public static String IMDB_Rating;
+	public static String Director_ID;
+	public static String Director_Name;
+	public static String Actor_ID;
+	public static String Actor_Name;
+	public static String Year;
 
-	
-	public MovieDataLayer() throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("C:/Everything/Capita/Workspace_Neon/IMDb/TestData.csv"));
+	static List<Director> Director_Names = new ArrayList<Director>();
+	static List<Actor> Actor_Names = new ArrayList<Actor>();
+
+
+	private static  void MovieDataLayer() {
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader("TestData.csv"));
 		String line = br.readLine(); // Reading header, Ignoring
 
 		while ((line = br.readLine()) != null && !line.isEmpty()) {
@@ -34,12 +42,35 @@ public class MovieDataLayer {
 			Film_Name = fields[1];
 			IMDB_Rating = fields[2];
 			Director_ID = fields[3];
-			Director_Nane = fields[4];
+			Director_Name = fields[4];
 			Actor_ID = fields[5];
 			Actor_Name = fields[6];
 			Year = fields[7];
+
+
+			Director d = new Director();
+			d.setDirectorName(Director_Name);
+			d.getDirectorName();
+			Director_Names.add(d);
+
+
+			Actor a = new Actor();
+			a.setActorName(Actor_Name);
+			a.getActorName();
+			Actor_Names.add(a);
+
+			//this is a test
+			//Film f= new Film (Film_ID,Film_Name,IMDB_Rating,Director_Names,Actor_Names,Year);
+			
 		}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
 }
 
 
